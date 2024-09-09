cd39b28836a02c5fc64769d32e1e2b5a93f34c03
Vin11293 <kvin11293@gmail.com>
2024-09-09 11:46:03 +0100

    challenge 5 update q

--- challenge5.py
+++ challenge5.py
@@ -1,19 +1,15 @@
 def only_ints(n, m):
     try:
         n = int(n)
-    except ValueError:
-        ans = "False"
-    try:
         m = int(m)
+        return True
     except ValueError:
-        ans = "False"
-    if type(n) is int and type(m) is int:
-        ans = "True"
-    else:
-        ans = "False"
-    return ans
+        return False
+   
 
 n = input("input a character/number")
 m = input("input a character/number")
 answ = only_ints(n, m)
 print ("Are both inputs integers?: ", answ)
+
+
